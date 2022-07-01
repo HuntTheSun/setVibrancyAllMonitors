e7b32024713325cae2f31baf3c6b5344322dbee2
HuntTheSun <boandlkramer793@gmail.com>
2022-07-01 18:04:27 +0200

    first commit, yeet :)

---
+++ setVibrancyAllMonitors.sh
@@ -0,0 +1,21 @@
+### DISCLAIMER: This is my first script, I dont know anything about scripting**  
+### This might not work on your system or have horrendous programming flaws**  
+
+## x11-vibrant-cli-setAll 
+### Description:
+This script allows you to change the color vibrancy of all of your screens simultaneously.  
+**Use case example**: 
+I launch the script with desired values via keybinding.  
+
+### Required packages:
+[libvibrant](https://gitlab.com/libvibrant/libvibrant)
+
+### Usage
+
+*Syntax*:  
+  `setVibrancyAllMonitors.sh yourDesiredValueHere`
+
+*Example*:  
+  `setVibrancyAllMonitors.sh 1.5`
+
+
